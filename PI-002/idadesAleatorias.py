d3d1be732685f694813ae569e1a7b5c5bebc4a4b
Vinicius <vini_lima98@outlook.com>
2023-12-18 11:21:04 -0300

    Ajustes

--- PI-002/idadesAleatorias.py
+++ PI-002/idadesAleatorias.py
@@ -13,3 +13,24 @@ class ListaIdade:
 quantidade_de_idades = 20
 idades = ListaIdade.geraListaIdades(quantidade_de_idades)
 print(idades.lista)
+
+"""
+import random
+
+class ListaIdade:
+    def __init__(self, idades):
+        self.idades = idades
+
+def geraListaIdade(n, iMin=18, iMax=65):
+    idades_geradas = []
+    for _ in range(n):
+        idade = random.randint(iMin, iMax)
+        idades_geradas.append(idade)
+    
+    return ListaIdade(idades_geradas)
+
+# Exemplo de uso:
+lista_idades = geraListaIdade(20)  # Gera uma lista de 5 idades com valores padrão
+print(lista_idades.idades)
+
+"""
--- PI-002/salariosAleatorios.py
+++ PI-002/salariosAleatorios.py
@@ -13,3 +13,24 @@ class ListaSalarios:
 quantidade_de_salarios = 20
 salarios = ListaSalarios.geraListaSalarios(quantidade_de_salarios)
 print(salarios.lista)
+
+
+"""
+import random
+
+class ListaSalario:
+    def __init__(self, salarios):
+        self.salarios = salarios
+
+def geraListaSalario(n, sMin=1000, sMax=5000):
+    salarios_gerados = []
+    for _ in range(n):
+        salario = random.uniform(sMin, sMax)
+        salarios_gerados.append(salario)
+    
+    return ListaSalario(salarios_gerados)
+
+lista_salarios = geraListaSalario(5)
+print(lista_salarios.salarios)
+
+"""
