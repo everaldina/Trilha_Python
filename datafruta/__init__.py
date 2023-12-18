8a18f23e6a97841eaf91f3ba776a42b8db9f4512
Ian Robert <iannrobert@hotmail.com>
2023-12-17 21:23:30 -0300

    Organizacao_em_pastas

---
+++ datafruta/__init__.py
@@ -0,0 +1,6 @@
+from . AnaliseDados import AnaliseDados
+from . Data import Data
+from . ListaIdades import ListaIdades
+from . ListaNomes import ListaNomes
+from . ListaSalarios import ListaSalarios
+from . ListaDatas import ListaDatas
