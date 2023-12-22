494932d5cc1f5e6569260c349f84526921a8d885
IanRobertLuzPinheiro <149024071+IanRobertLuzPinheiro@users.noreply.github.com>
2023-12-21 21:46:20 -0300

    Update AnaliseDados.py
    Adição dos novos métodos:
    - Médias e medidas de valor central;
    - Medidas de espalhamento;
    - Métodos da classe ListaNotas

--- DataFruta_V1/AnaliseDados.py
+++ DataFruta_V1/AnaliseDados.py
@@ -21,3 +21,76 @@ class AnaliseDados(ABC):
     @abstractmethod
     def listarEmOrdem(self):
         pass
+
+    
+    #Média e medidas de valor central
+    
+    @abstractmethod
+    def mediaAritmetica(self):
+        pass
+    
+    @abstractmethod
+    def mediaGeometrica(self):
+        pass
+    
+    @abstractmethod
+    def mediaHarmonica(self):
+        pass
+    
+    @abstractmethod
+    def medianaInferior(self):
+        pass
+    
+    @abstractmethod
+    def medianaSuperior(self):
+        pass
+
+
+    #Medidas de espalhamento
+
+    @abstractmethod
+    def desvioPadraoPopulacional(self):
+        pass
+    
+    @abstractmethod
+    def varianciaPopulacional(self):
+        pass
+    
+    @abstractmethod
+    def desvioPadraoAmostral(self):
+        pass
+    
+    @abstractmethod
+    def varianciaAmostral(self):
+        pass
+
+    
+    # Métodos próprios para a classe ListaNotas
+    
+    @abstractmethod
+    def alunosComNotaMaiorQue(self, valor):
+        pass
+
+    @abstractmethod
+    def alunosComNotaMenorQue(self, valor):
+        pass
+    
+    @abstractmethod
+    def aproximaNotasSete(self):
+        pass
+    
+    @abstractmethod
+    def calcularModa(self):
+        pass
+    
+    @abstractmethod
+    def alunosParaFinal(self):
+        pass
+    
+    @abstractmethod
+    def alunosSemNotaParaFinal(self):
+        pass
+    
+    @abstractmethod
+    def alunosAprovados(self):
+        pass
