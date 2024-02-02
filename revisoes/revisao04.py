82ede18a4b47d57b3faa0a7b63a11ac71e1a8eca
nairansantos <nairanb28@gmail.com>
2024-02-02 09:47:36 -0300

    add simular_trajetoria

---
+++ revisoes/revisao04.py
@@ -0,0 +1,17 @@
+
+
+def simular_trajetoria():
+    # Tamanho da matriz
+    tamanho_matriz = 400
+
+    # Posição inicial do bar
+    i, j = 200, 200
+
+    # Inicializar a 
+    visitas = np.zeros((400, 400))
+
+    # Número máximo de passos por simulação
+    max_passos = 500
+
+
+  
