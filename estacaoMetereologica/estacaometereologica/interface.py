1fc86d96e89aaa47c1e8397dae5832060613f999
João Manoel <jmaoliveira.cic@uesc.br>
2024-02-23 15:48:21 -0300

    Versão 0.1.7

--- estacaoMetereologica/estacaometereologica/interface.py
+++ estacaoMetereologica/estacaometereologica/interface.py
@@ -1,50 +1,48 @@
 import tkinter as tk
 from tkinter import messagebox, ttk
 from estacaometereologica.search import Search
+import webbrowser
 
 class Interface(tk.Tk):
     def __init__(self, anos = None):
         super().__init__()
 
         self.title('Estação Metereológica')
-        self.geometry('400x200')
+        self.resizable(False, False)
 
         self.anoSelecionado = tk.StringVar()
         self.estacaoSelecionada = tk.StringVar()
 
-        self.grid_columnconfigure([1,2], weight=1)
+        self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(1, weight=1)
 
         self.frameEstacao = tk.Frame(self)
-        self.frameEstacao.grid(row=1, column=1, sticky='nsew')
-
-        self.frameGrafico = tk.Frame(self)
-        self.frameGrafico.grid(row=1, column=2, sticky='nsew')
+        self.frameEstacao.grid(row=1, column=1, sticky='nsew', padx=5, pady=5)
         
-        self.frameEstacao.grid_rowconfigure([1,2,3,4,5,6,7,8], weight=1)
-        self.frameEstacao.grid_columnconfigure([1,2,3,4,5], weight=1)
+        self.frameEstacao.grid_rowconfigure([1,2,3,4,5], weight=1)
+        self.frameEstacao.grid_columnconfigure([1,2], weight=1)
 
         self.frameEstacao.lblAnos = tk.Label(self, text='Escollha a estação: ')
-        self.frameEstacao.lblAnos.grid(row=0, column=1, padx=5, pady=5)
+        self.frameEstacao.lblAnos.grid(row=2, column=1)
 
         self.frameEstacao.inpAnos = ttk.Combobox(self, values=[],
                                                   state='readonly', textvariable=self.anoSelecionado)
         self.frameEstacao.inpAnos.bind('<<ComboboxSelected>>', self.seleciona_ano)
-        self.frameEstacao.inpAnos.grid(row=1, column=1, padx=5, pady=5)
+        self.frameEstacao.inpAnos.grid(row=2, column=2, padx=5, pady=5)
 
         self.frameEstacao.btnCarregar = tk.Button(self, text='Carregar', command=self.carregar)
-        self.frameEstacao.btnCarregar.grid(row=2, column=1, padx=5, pady=5)
+        self.frameEstacao.btnCarregar.grid(row=3, column=2, padx=5, pady=5)
 
         self.frameEstacao.lblEstacao = tk.Label(self, text='Escollha o ano: ')
-        self.frameEstacao.lblEstacao.grid(row=3, column=1, padx=5, pady=5)
+        self.frameEstacao.lblEstacao.grid(row=4, column=1, padx=5, pady=5)
 
         self.frameEstacao.inpEstacoes = ttk.Combobox(self, values=[], 
                                                      state='readonly', textvariable=self.estacaoSelecionada)
         self.frameEstacao.inpEstacoes.bind('<<ComboboxSelected>>', self.seleciona_estacao)
-        self.frameEstacao.inpEstacoes.grid(row=4, column=1, padx=5, pady=5)
+        self.frameEstacao.inpEstacoes.grid(row=4, column=2, padx=5, pady=5)
 
         self.frameEstacao.btnMostarGrafico = tk.Button(self, text='Mostar grafico', command=None)
-        self.frameEstacao.btnMostarGrafico.grid(row=5, column=1, padx=5, pady=5)
+        self.frameEstacao.btnMostarGrafico.grid(row=5, column=2, padx=5, pady=5)
 
         self.carregar_anos()
 
@@ -53,17 +51,27 @@ class Interface(tk.Tk):
             self.pesquisa = Search()
             self.anos = self.pesquisa.get_anos()
             self.anosList = list(self.anos.keys())
+            self.anosList.insert(0, '')
 
             self.frameEstacao.inpAnos['values'] = self.anosList
+        except ConnectionError:
+            messagebox.showerror('Erro', 'Não foi possível conectar ao servidor')
         except Exception as e:
-            messagebox.showerror('Erro', f'Erro de conexão')
+            messagebox.showerror('Erro', e)
 
     def seleciona_ano(self, event):
-        print('Ano selecionado:')
-        print(self.anoSelecionado.get())
+        pass
 
     def seleciona_estacao(self):
         pass
-
+    
     def carregar(self):
-        print(f"link: {self.anos[self.anoSelecionado.get()]}")
+        if self.anoSelecionado.get() == '':
+            messagebox.showwarning('Aviso', 'Selecione um ano')
+        else:
+            popup = tk.Toplevel(self)
+            popup.title(f"Link ano {self.anoSelecionado.get()}")
+            
+            label = tk.Label(popup, text=f"{self.anos[self.anoSelecionado.get()]}", fg="blue", cursor="hand2")
+            label.pack(padx=20, pady=20)
+            label.bind("<Button-1>", lambda e: webbrowser.open_new(self.anos[self.anoSelecionado.get()]))
