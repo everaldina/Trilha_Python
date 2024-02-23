b0b402fe45ab7f6d77f1a877885c894925c36a1f
Everaldina G. Barbosa <everaldinag@gmail.com>
2024-02-23 10:11:44 -0300

    Versão 0.1.3

--- estacaoMetereologica/estacaometereologica/search.py
+++ estacaoMetereologica/estacaometereologica/search.py
@@ -5,21 +5,29 @@ class Search:
     def __init__(self):
         self.link = 'https://portal.inmet.gov.br/dadoshistoricos'
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-        self.requisicao = requests.get(self.link, headers=self.headers)
+        try:
+            self.requisicao = requests.get(self.link, headers=self.headers)
+        except ConnectionError:
+            self.requisicao = None
+            self.resposta = None
+            raise Exception('Requisição não foi bem sucedida')
+        except Exception as e:
+            self.requisicao = None
+            self.resposta = None
+            raise e
         if self.requisicao:
             self.resposta = bs4.BeautifulSoup(self.requisicao.text, 'html.parser')
         else:
             self.resposta = None
     
     def get_anos(self):
-        if self.self.resposta:
+        if self.resposta:
             lista_anos = []
             lita_links = self.resposta.find('div', attrs={'id': 'main'}).find_all('a')
             for item in lita_links:
                 link = item.attrs['href']
                 ano = link.split('/')[-1]
-                lista_anos.append({'ano': ano,  'link': link}) # Adiciona tupla com o ano e o link do arquivo .zip
+                lista_anos.append({'ano': ano[:-4],  'link': link}) # Adiciona tupla com o ano e o link do arquivo .zip
             return lista_anos
         else:
             raise Exception('Requisição não foi bem sucedida')
-    
